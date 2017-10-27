5da0dd917f319395c718da81a6bef8f6f2d3b49d
Linda Fernandez <lindafernandez@Lindas-MacBook-Pro.local>
2017-10-26 20:31:17 -0400

    Added VerfiyHeadShake

--- src/main/java/org/ufl/cnt4007/Handshake.java
+++ src/main/java/org/ufl/cnt4007/Handshake.java
@@ -1,5 +1,6 @@
 package org.ufl.cnt4007;
 
+import java.util.Arrays;
 import java.util.BitSet;
 import java.nio.ByteBuffer;
 
@@ -76,6 +77,25 @@ public class Handshake {
 		bytes.putInt(id);
 		return bytes.array();
 	}
+	
+	public static boolean verifyHandshake(byte[] b, int peerID){
+		
+		byte[] id = new byte[4];
+		System.arraycopy(b, b.length - 4, id, 0, id.length);
+		ByteBuffer bbID = ByteBuffer.wrap(id);
+		int pId = bbID.getInt();
+		
+		byte[] headerCheck = new byte[18];
+		System.arraycopy(b, 0, headerCheck, 0, headerCheck.length);
+		//ByteBuffer bbHeader = ByteBuffer.wrap(header);
+		Arrays.equals(header, headerCheck);
+		
+		if (Arrays.equals(header, headerCheck) && pId == peerID){
+			return true;
+		}
+		return false;
+	}
+	
 	public static Handshake getHandshake(ByteBuffer byteBuffer){
 		Handshake handshake = new Handshake();
 		byte[] b = byteBuffer.array();
