456d6e131b9a90785ca330132aea74528ca30ed4
jwziggy <jamesziegenfelder@gmail.com>
2017-10-27 17:08:16 -0400

    checking payload length before debugging statement

--- src/main/java/org/ufl/cnt4007/ActualMsg.java
+++ src/main/java/org/ufl/cnt4007/ActualMsg.java
@@ -23,8 +23,10 @@ public class ActualMsg {
 	
 	public ActualMsg(byte[] payload) {
 		//ByteBuffer bytes = ByteBuffer.wrap(payload);
-		for (byte b : payload) {
-			System.out.print(b + " ");
+		if(payload.length > 1) {
+			for (byte b : payload) {
+				System.out.print(b + " ");
+			}
 		}
 		System.out.println();
 		this.msgType = typeValues[payload[0]];
